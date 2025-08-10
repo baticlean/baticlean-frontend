b030fada69e11db079adcf03488c19940c50a4d0
baticlean <baticlean225@gmail.com>
2025-08-10 16:05:41 +0000

    correction bookingslice et creation viteconfig

---
+++ vite.config.js
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'BATIClean',
+        short_name: 'BATIClean',
+        description: 'Services de nettoyage et de maintenance de bâtiments.',
+        theme_color: '#8A2387',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
+})
