02e63ed3cb81804d8e66a81ce82fbbd3e3aee468
baticlean <baticlean225@gmail.com>
2025-08-13 11:07:48 +0000

    Correction apk

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 // vite.config.js
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 import versionInjector from './version-injector-plugin.js';
 
 export default defineConfig({
@@ -15,28 +15,30 @@ export default defineConfig({
         short_name: 'BATIClean',
         description: 'Services de nettoyage et de maintenance de bâtiments.',
         theme_color: '#8A2387',
-        // ✅ ON AJOUTE CETTE LIGNE POUR DIRE À L'APPLI OÙ COMMENCER
-        start_url: '/', 
+        background_color: '#ffffff',
+        display: 'standalone',
+        scope: '/',
+        start_url: '/',
         icons: [
           {
             src: 'pwa-192x192.png',
             sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
+            type: 'image/png',
           },
           {
             src: 'pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png',
-            purpose: 'maskable'
-          }
-        ]
-      }
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
     }),
     versionInjector(),
   ],
-})
+});
